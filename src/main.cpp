a83dcfcda52f0b4b30c16074ad155c9e7a36ca03
tom_glb <goodMoring_glb@atguigu.com>
2022-07-24 17:02:10 +0800

    添加项目文件。

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <signal.h>
+#include "src/ping.h"
+
+Ping* p;
+
+void SingnalHandler(int signo) {
+
+    p->statistic();
+
+    exit(0);
+}
+
+int main(int argc, char* argv[]) {
+    struct sigaction action;
+
+    action.sa_handler = SingnalHandler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+
+    sigaction(SIGINT, &action, NULL);
+
+    Ping ping(argv[1], 1);
+    p = &ping;
+    ping.CreateSocket();
+    while (1)
+    {
+        ping.SendPacket();
+        ping.RecvPacket();
+        sleep(1);
+    }
+}
